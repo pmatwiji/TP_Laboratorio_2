fad2ebaa099ce150b3a026ae6acf5667705790d3
pmatwiji <pmatwiji@gmail.com>
2019-11-27 20:53:18 -0300

    Tp 4 terminado

---
+++ Matwijiszyn.Pablo.2A_TP4/Entidades/Correo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Correo : IMostrar<List<Paquete>>
+    {
+        #region Atributos
+
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+
+        #endregion
+
+        #region Propiedades
+
+        public List<Paquete> Paquetes { get => this.paquetes; set => this.paquetes = value; }
+
+        #endregion
+
+        public Correo()
+        {
+            this.mockPaquetes = new List<Thread>();
+            this.paquetes = new List<Paquete>();
+        }
+
+        #region Metodos
+
+        /// <summary>
+        /// Guarda en una cadena todos los paquetes de un correo
+        /// </summary>
+        /// <param name="elemento"></param>
+        /// <returns>string con datos de los paquetes de correo</returns>
+        public string MostrarDatos(IMostrar<List<Paquete>> elemento)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Paquete item in ((Correo)elemento).paquetes)
+            {
+                sb.AppendFormat("{0} para {1} ({2}) \n", item.TrackingID, item.DireccionEntrega, item.Estado.ToString());
+
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Cierra todos los hilos activos
+        /// </summary>
+        public void FinEntregas()
+        {
+            foreach (Thread item in this.mockPaquetes)
+            {
+                item.Abort();
+            }
+        }
+
+        public static Correo operator +(Correo c, Paquete p)
+        {
+
+            foreach (Paquete item in c.paquetes)
+            {
+                if (item == p)
+                {
+                    throw new TrackingIdRepetidoException("El Tracking ID " + item.TrackingID + " ya se encunetra en la lista.");
+                }
+            }
+
+            c.paquetes.Add(p);
+
+            Thread nuevoThread = new Thread(p.MockCicloDeVida);
+
+            c.mockPaquetes.Add(nuevoThread);
+
+            nuevoThread.Start();
+
+            return c;
+        }
+
+        #endregion
+    }
+}
---
+++ Matwijiszyn.Pablo.2A_TP4/Entidades/PaqueteDAO.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Entidades
+{
+    public static class PaqueteDAO
+    {
+        #region Atributos
+
+        private static SqlCommand comando;
+        private static SqlConnection conexion;
+
+        #endregion
+
+        static PaqueteDAO()
+        {
+            comando = new SqlCommand();
+            conexion = new SqlConnection(Properties.Settings.Default.conexion);
+            comando.Connection = conexion;
+        }
+
+        #region Metodos
+
+        /// <summary>
+        /// Metodo que inserta un "paquete" en la base de datos correo-sp-2017
+        /// </summary>
+        /// <param name="p">Objeto a insertar</param>
+        /// <returns>true si pudo insertar el objeto , false si no</returns>
+        public static bool Insertar(Paquete p)
+        {
+            bool retorno = false;
+            comando.CommandText = string.Format("INSERT INTO Paquetes values ('{0}','{1}','{2}')", p.DireccionEntrega, p.TrackingID, "Pablo Matwijiszyn");
+            comando.CommandType = CommandType.Text;
+
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                retorno = true;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return retorno;
+        }
+
+        #endregion
+    }
+}
